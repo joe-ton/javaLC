9e356284fe8139546e865db7fe2b83a6d20543e2
joe-ton <joeton.public@gmail.com>
2024-04-19 12:26:30 -0700

    javaLC: reset practice

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        int[] nums = {1, 2, 3, 1};
+        int target = 5;
+        Solution solution = new Solution();
+        int[] result = solution.getTwoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+class Solution {
+    public int[] getTwoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalAccessError("Hello, Wrong");
+    }
+}
